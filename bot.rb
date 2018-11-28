53d373a2b1050f85a45ebeaf09c233dee17040c0
alex <alex_is_ok@yahoo.com>
2018-11-28 07:26:24 +0200

    first bot

---
+++ bot.rb
@@ -0,0 +1,20 @@
+require 'telegram/bot'
+
+TOKEN = '760983628:AAHqK5HTBc-nqGBIVYxzsFUWGUCQrY4NeZY'
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start', 'start'
+      bot.api.send_message(
+          chat_id: message.chat.id,
+          text: "Пирювэд, #{message.from.first_name}.\n" + "Спрашивай, дарагой..."
+      )
+    else
+      sleep 1
+      bot.api.send_message(
+          chat_id: message.chat.id,
+          text: 'Да что ты гаваришь такоэ??..')
+      end
+    end
+  end
